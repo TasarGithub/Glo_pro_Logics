4ebaade0523c7f32537b2ba92d659ac1eff160f3
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-10-30 15:15:09 +0300

    end

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,24 @@
 'use strict';
 // logics191030
-function functionTrain(speedTrain1, speedTrain2, distance){
-  
-  let timeNum = distance / (speedTrain1+speedTrain2),
-       timeHour = Math.trunc(timeNum),
-       timeMin = Math.trunc((timeNum - timeHour) * 60),
-       timeSec = Math.floor(((timeNum - timeHour) * 60 - timeMin)* 60);
-  return timeHour + 'hh ' + timeMin + 'mm ' + timeSec + 'ss';
+// Для каждого элемента X данного массива подсчитайте, сколько элементов справа от X
+// меньше, чем X.
+// Последнее значение всегда равно нулю, поскольку справа от последнего элемента нет
+// элементов.
 
+function amountSmaller(arr) {
+  let arrSmaller = arr.filter(function(v,i,ar){
+    return ar[0]>v;
+  });
+  return arrSmaller.length;
+} 
+
+function funcNum(arrNum){
+  //let arrNum = [5,4,4,6,1],
+
+  for (let i = 0; i < arrNum.length; i++ ){
+    arrNum[i] = amountSmaller (arrNum.slice(i));
+  }
+  console.log('arrResult: ', arrNum);
 }
-console.log ('Время до встречи двух поездов: ' + functionTrain(60, 80, 804));
 
-
-  //sdsdsd
+funcNum([5,4,3,2,1]);
