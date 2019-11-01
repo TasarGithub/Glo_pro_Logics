c09fe5cd4c32e5ddef2579d25503067649bf0d77
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-01 20:08:51 +0300

    logics03-191101  done

--- Script/script.js
+++ Script/script.js
@@ -1,37 +1,39 @@
 'use strict';
-// logics191030
-// Для каждого элемента X данного массива подсчитайте, сколько элементов справа от X
-// меньше, чем X.
-// Последнее значение всегда равно нулю, поскольку справа от последнего элемента нет
-// элементов.
+// logics191101
+// Написать функцию, которая рассчитает угол между часовой и минутной стрелками
+// часов!
+// Функция принимает два аргумента (часы, минуты)
 
+// например
+// func(6, 0) =>>> 180
+// func(3, 0) =>>> 90
+// func(3, 30) =>>> 75
 
 let funcDegree = function (h,m){
-  const degreeInHour = 360/12,
-        degreeInMin = 360/60,
-        //коррекция на прохождение часовой стрелки, от точного значения в течении часа.
-        correctionHour = (5 * (m / 60 )),
-   //console.log('correctionHour: ', correctionHour.toFixed(1));
-        degM = m*degreeInMin ,
-        degH = h*degreeInHour + correctionHour.toFixed(1)*degreeInMin;
-    console.log('degM: ', degM);
-    console.log('degH: ', degH);
-    let result;
+  //коррекция на прохождение часовой стрелки, от точного значения в течении часа.
+  const correctionHour = 5 * (m / 60 ),
+  //градусы минутной стрелки
+        degM = m * 360 / 60 ,
+  //градусы часовой стрелки
+        degH = h * 360 / 12 + correctionHour.toFixed(1)* 360 / 60;
+  
+  // Cчитаем угол от часовой стрелки к минутной в направлению по часовой.
   // мин впереди часовой
   if  (degM >= degH) {
-    return degM - degH;
+    return (degM - degH).toFixed(1);
   // часовая впереди мин
   } else if (degM < degH){
-    return degH + degM; 
+    return (360 - (degH - degM)).toFixed(1); 
   } 
 }
 
-console.log('funcDegree(10,45);: ', funcDegree(10,45));
 
-console.log('funcDegree(11, 05);: ', funcDegree(11, 5));
+console.log('время: 01:06: ', funcDegree(1,6));
+console.log('время: 06:48: ', funcDegree(6,48));
+console.log('время: 11:05: ', funcDegree(11,5));
+console.log('время: 5:35: ', funcDegree(5,35));
 
-console.log('funcDegree(3,50);: ', funcDegree(3,50));
-
-console.log('funcDegree(6,10);: ', funcDegree(6,10));
-
-console.log('funcDegree(7,17);: ', funcDegree(7,17));
+console.log('время: 3:50: ', funcDegree(3,50));
+console.log('время: 6:10: ', funcDegree(6,25));
+console.log('время: 7:1: ', funcDegree(7, 1));
+console.log('время: 10:05: ', funcDegree(10,5));
