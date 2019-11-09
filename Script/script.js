c82dd0abbe1250edd32996b47eaae4fac53e5700
Taras Vyshegorodtsev <tasarvv@gmail.com>
2019-11-09 20:08:54 +0300

    logics04-191108 done

--- Script/script.js
+++ Script/script.js
@@ -1,39 +1,44 @@
 'use strict';
-// logics191101
-// Написать функцию, которая рассчитает угол между часовой и минутной стрелками
-// часов!
-// Функция принимает два аргумента (часы, минуты)
+// logics04191108
+// Есть цепочка ДНК, верните ее комплемент РНК. Обе цепи ДНК и РНК представляют
+// собой последовательность нуклеотидов.
+// Нуклеотидами ДНК являются аденин (А), цитозин (С), гуанин (G) и тимидин (Т).
+// Нуклеотидами РНК являются аденин (A), цитозин (C), гуанин (G) и урацил (U).
+// Транскрибируемая цепь РНК цепи ДНК образуется путем замены каждого нуклеотида
+// его комплементом: G -> C, C -> G, T -> A, A -> U.
+// написать функцию которая будет принимать ДНК и выдавать РНК
 
 // например
-// func(6, 0) =>>> 180
-// func(3, 0) =>>> 90
-// func(3, 30) =>>> 75
-
-let funcDegree = function (h,m){
-  //коррекция на прохождение часовой стрелки, от точного значения в течении часа.
-  const correctionHour = 5 * (m / 60 ),
-  //градусы минутной стрелки
-        degM = m * 360 / 60 ,
-  //градусы часовой стрелки
-        degH = h * 360 / 12 + correctionHour.toFixed(1)* 360 / 60;
-  
-  // Cчитаем угол от часовой стрелки к минутной в направлению по часовой.
-  // мин впереди часовой
-  if  (degM >= degH) {
-    return (degM - degH).toFixed(1);
-  // часовая впереди мин
-  } else if (degM < degH){
-    return (360 - (degH - degM)).toFixed(1); 
-  } 
-}
+// func("ACGTGGTCTTAA");
+// результат =>>> "UGCACCAGAAUU" 
 
 
-console.log('время: 01:06: ', funcDegree(1,6));
-console.log('время: 06:48: ', funcDegree(6,48));
-console.log('время: 11:05: ', funcDegree(11,5));
-console.log('время: 5:35: ', funcDegree(5,35));
+let funcDnkToRnk = function (dnk){
+  let rnk = '';
+  for (let chr of dnk){
+    chr = chr.toUpperCase();
+    switch (chr) {
+      case 'G':
+        rnk +='C';
+        break;
+      case 'C':
+        rnk +='G';
+        break;
+      case 'T':
+        rnk +='A';
+        break;
+      case 'A':
+        rnk +='U';
+        break;        
+      default:
+        console.log('Недопустимый символ ' + chr);
+        //alert ('Недопустимый символ' + chr);
+        break;
+    }
+  }
+  return rnk;
+};
 
-console.log('время: 3:50: ', funcDegree(3,50));
-console.log('время: 6:10: ', funcDegree(6,25));
-console.log('время: 7:1: ', funcDegree(7, 1));
-console.log('время: 10:05: ', funcDegree(10,5));
+
+console.log(funcDnkToRnk('ACHGTGGTCTTAA'));
+
